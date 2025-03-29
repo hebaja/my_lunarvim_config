665c04f3ca38eec2efea1b62e4102aacc9a44bb7
Henrique <hebaja@yahoo.com.br>
2025-03-28 21:29:06 -0300

    commenting for dealing with tab

--- config.lua
+++ config.lua
@@ -3,6 +3,9 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+-- ATTENTION!!! IN FILE: ~/.local/share/lunarvim/lvim/lua/lvim/core/cmp.lua
+-- COMMENT FROM LINE 297 TO 319 TO AVOID TAB KEY TO NAVIGATE THROUGH THE AUTOCOMPLETE MENU
 
 -- Use tabs instead of spaces
 vim.opt.expandtab = false
